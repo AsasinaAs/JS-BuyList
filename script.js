be6a2c10ad1c3c228994d2ada7b2132b1744be1b
Fedchuk Anastasia <asya.fedchuk@gmail.com>
2019-02-27 01:14:17 +0200

    [practice-3] configuration of bottons add and delete

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 $(function(){
 	
+	var 
+	
 	$('#inputbutton').click(function()
 	{
 		 if ($('#inputline').val())
 		{
-		   var productName = $('#inputline').val();
+		var productName = $('#inputline').val();
+		var productNumber = 1;
 				
 		const PRODUCT_TEMPLATE = $('#product_template');
 		const PRODUCT_LEFT = $('#product_a_left');
@@ -15,6 +18,39 @@ $(function(){
 		$clone1.find('.product').text(productName);
 		$clone2.find('.p_item').text(productName);
 
+		$clone1.find('.add').click(function()
+		{
+			if(productNumber==1)
+			{
+				$clone1.find('.delete').prop('disabled', false);
+				$clone1.find('.delete').css('opacity','1');
+			}
+			productNumber++;
+			$clone2.find('.product-amount-left').text(productNumber);
+			$clone1.find('.product-amount').text(productNumber);
+		})
+		
+		var disable_butt = function()
+		{
+			$clone1.find('.delete').prop('disabled', true);
+			$clone1.find('.delete').css('opacity','0.6');
+		}
+		disable_butt();
+		
+		$clone1.find('.delete').click(function()
+		{
+			if(productNumber>1)
+			{
+				if(productNumber==2)
+				{
+				disable_butt();
+				}
+				productNumber--;
+				$clone2.find('.product-amount-left').text(productNumber);
+			    $clone1.find('.product-amount').text(productNumber);
+			}
+		});
+		
     	$('#product_items').append($clone1);
 		$clone1.slideDown("slow");
 		$('#all_products_left').append($clone2);
@@ -30,12 +66,5 @@ $(function(){
           $('#inputbutton').click();
        }
     });
-	
-	$('.goods').click(function()
-	{
 		
-		
-		$('.bottons').find('.product-amount').text($('.bottons').find('.product-amount').text()++);
-	});
-	
 });
